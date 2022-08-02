053afcfd0bb7a47d5891e1c873e70fd2c28cc1ab
jbmil <jbmiller015@gmail.com>
2022-08-01 20:05:31 -0400

    Style Education accordion content

--- src/components/showCase/resume/Education.js
+++ src/components/showCase/resume/Education.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import useComponentVisible from "../../../Hooks/useComponentVisible";
 import "./style/education.css"
+import LCLogo from '../../../Resources/images/LCLogo.png';
+import SULogo from '../../../Resources/images/SULogo.png';
+import moment from "moment";
 
 const Education = (props) => {
     const {ref, isComponentVisible, setIsComponentVisible} = useComponentVisible(false);
     const {school, startDate, endDate, location, field, degree} = props.values;
 
-    const renderComps = () => {
-        return Object.entries(props.values).map((entry, i) => {
-            return (<div className={"ui segment"} key={"eduShow" + i}>
-                <div className={"ui header"}>{entry[0]}</div>
-                <div className={"description"}>
-                    <p>{entry[1]}</p>
-                </div>
-            </div>)
-        })
+
+    const imageName = () => {
+      if(school === "Luther College"){
+         return <img src={LCLogo} className={"accordionImage"} alt={"LCLogo"}/>
+      }
+      else return <img src={SULogo} className={"accordionImage"} alt={"SULogo"}/>
     }
 
     const accordion = () => {
@@ -26,15 +26,19 @@ const Education = (props) => {
                 {degree + ": " + field}
             </div>
             <div className={`${isComponentVisible ? "active" : ""} content`} ref={ref}>
-                <div className={"lutherAccordionContent"}>
-                    <h4>{school}</h4>
-                    <p>{location}</p>
-                    <p>{startDate}</p>
-                    <p>{endDate}</p>
+                <div className={"accordionContent"}>
+                    {imageName()}
+                    <div className={"accordionText"}>
+                        <h2>{school}</h2>
+                        <p>{location}</p>
+                        <br/>
+                        <p>From</p>
+                        <p>{moment(startDate).format("MMMM YYYY")}</p>
+                        <p>To</p>
+                        <p>{moment(endDate).format("MMMM YYYY")}</p>
+                    </div>
                 </div>
-
             </div>
-
         </div>);
     }
     return (<div className={"ui segments"}>{accordion()}
