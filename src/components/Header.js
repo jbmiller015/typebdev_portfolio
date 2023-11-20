1ac3a6573ae5ced2b4963384cb92e91f6e3dc7be
Jacob Miller <jbmiller015@gmail.com>
2023-11-20 15:04:45 -0500

    New UX concept: Change section headers to be unique

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ function Header() {
             <h1>Jacob Miller</h1>
             <nav>
                 <ul>
-                    <li><a href={'home'}>Home</a></li>
-                    <li><a href={'about'}>About</a></li>
-                    <li><a href={'projects'}>Projects</a></li>
-                    <li><a href={'contact'}>Contact</a></li>
+                    <li><a className={"homeButton"} href={'home'}>Home</a></li>
+                    <li><a className={"aboutButton"} href={'about'}>About</a></li>
+                    <li><a className={"projectsButton"} href={'projects'}>Projects</a></li>
+                    <li><a className={"contactButton"} href={'contact'}>Contact</a></li>
                 </ul>
             </nav>
         </div>
