52f01f2a44d7a954c27a69b663d16582a8e29fbf
jbmil <jbmiller015@gmail.com>
2024-05-18 16:11:15 -0400

    Implemented concept text button

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ function Header() {
             <h1>Jacob Miller</h1>
             <nav>
                 <ul>
-                    <li><a className={"homeButton"} href={'home'}>Home</a></li>
-                    <li><a className={"aboutButton"} href={'about'}>About</a></li>
-                    <li><a className={"projectsButton"} href={'projects'}>Projects</a></li>
-                    <li><a className={"contactButton"} href={'contact'}>Contact</a></li>
+                    <li><a className={"HomeButton"} href={'home'}>Home</a></li>
+                    <li><a className={"AboutButton"} href={'about'}>About</a></li>
+                    <li><a className={"ProjectsButton"} href={'projects'}>Projects</a></li>
+                    <li><a className={"ContactButton"} href={'contact'}>Contact</a></li>
                 </ul>
             </nav>
         </div>
---
+++ src/components/projects/projectHome.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const projectHome = () => {
+
+
+  return(<div>projectHome</div>)
+}
+
+export default projectHome();
